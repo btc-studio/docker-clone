9db5c9f201d7682393a7ab377ab25e4c606421fb
NguyenTrong-Du <dunt@btc-studio.io>
2022-03-19 16:44:46 +0700

    updated

--- components/Layout/MenuIcon.tsx
+++ components/Layout/MenuIcon.tsx
@@ -1,7 +1,7 @@
 import { MenuIconProps } from './interfaces';
 
 export default function MenuIcon({ isHome, open, setIsOpen }: MenuIconProps) {
-  const color = isHome || open ? 'bg-white' : 'bg-sky-700';
+  const color = isHome ? 'bg-white' : 'bg-sky-700';
   return (
     <div
       id="nav-icon"
